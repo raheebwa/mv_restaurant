d81e31ce4c623a1002428f25dff0a7d6d44e0ba9
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-13 03:24:52 +0300

    UI: Test webpack with index.html. close #4

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const wrapper = document.getElementById("content");
+
+
+const helloButton = document.createElement("button");
+helloButton.classList.add = ('btn', 'btn-primary');
+helloButton.setAttribute('content', 'Hello Dear');
+helloButton.innerHTML = 'Hello Dear';
+helloButton.onclick = alert("Hiiiiii!");
+
+wrapper.appendChild(helloButton);
