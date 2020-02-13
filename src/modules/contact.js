6e9a64ae51cd78f82e5eb570ea1b25b29a4dfadc
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-14 02:04:41 +0300

    UI: Render Contact page, ref #6

---
+++ src/modules/contact.js
@@ -0,0 +1,33 @@
+// 1. Render container
+const container = document.createElement('div');
+container.setAttribute('class', 'text-center');
+container.setAttribute('id', 'home');
+
+//  2. Render Paragraph
+const paragraph = (content) => {
+    const p = document.createElement('p');
+    p.setAttribute('class', 'lead');
+    p.innerText = content;
+    return p;
+}
+
+//  2.1 REnder contact link
+  const mailto = document.createElement('a');
+  mailto.setAttribute('href', 'mailto:mail@raheebwa.com');
+  mailto.setAttribute('class', 'link display-4 text-center');
+  mailto.innerText = 'Email us here';
+
+//  3. Data to render
+let data = ['Feel free to contact us, Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates maxime animi natus officiis?'];
+
+//  4. Function to render page 
+
+function renderContactPage() {
+    data.forEach(item => {
+        container.appendChild(paragraph(item));
+    });
+    container.appendChild(mailto);
+    return container;
+}
+
+export default renderContactPage;
