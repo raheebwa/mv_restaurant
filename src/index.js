cbb778ba6a5bb3ce3860704514da6d93592a1156
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-14 02:06:16 +0300

    UI: Link all pages to index.js. close #6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 
 import renderBanner from './modules/banner';
 import renderNav from './modules/navigation';
+import renderHomePage from './modules/home';
+import renderMenuPage from './modules/menu';
+import renderContactPage from './modules/contact';
 
 const myHeader = document.getElementById('content');
 myHeader.setAttribute('class', 'container');
 myHeader.appendChild(renderBanner());
 myHeader.appendChild(renderNav());
-
-// console.log(renderNav());
