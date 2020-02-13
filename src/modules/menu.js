3f3e0855222205f16a019cd5b316f6d2ec266e18
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-14 01:01:04 +0300

    UI: Render Card Container

---
+++ src/modules/menu.js
@@ -0,0 +1,14 @@
+// 1. Render Card columns container
+const cardContainer = document.createElement('div');
+cardContainer.setAttribute('card-columns');
+// 2. Render Card 
+
+//  3. Render Card Body
+
+//  4. Render Card Content
+
+//  5. Data for Cards
+
+//  6. Function to join them together
+
+//  7. Export Menu Page
