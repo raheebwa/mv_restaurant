cfb3206b61e5756b6c57711ff95e8f4d5956378b
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-13 03:46:47 +0300

    UI: Add barebonme design, resolve #5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
-const wrapper = document.getElementById("content");
+// const wrapper = document.getElementById("content");
 
 
-const helloButton = document.createElement("button");
-helloButton.classList.add = ('btn', 'btn-primary');
-helloButton.setAttribute('content', 'Hello Dear');
-helloButton.innerHTML = 'Hello Dear';
-helloButton.onclick = alert("Hiiiiii!");
+// const helloButton = document.createElement("button");
+// helloButton.classList.add = ('btn', 'btn-primary');
+// helloButton.setAttribute('content', 'Hello Dear');
+// helloButton.innerHTML = 'Hello Dear';
+// helloButton.onclick = alert("Hiiiiii!");
 
-wrapper.appendChild(helloButton);
+// wrapper.appendChild(helloButton);
