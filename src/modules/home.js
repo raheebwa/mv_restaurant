bb8b9b461f66e94dd01eedcd717a79acfc5296dd
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-14 01:54:16 +0300

    UI: Add Home page content, ref #6

---
+++ src/modules/home.js
@@ -0,0 +1,30 @@
+// 1. Render container
+const container = document.createElement('div');
+container.setAttribute('class', 'text-center');
+container.setAttribute('id', 'home');
+
+//  2. Render Content
+const paragraph = (content) => {
+    document.createElement('p');
+    paragraph.setAttribute('class', 'lead');
+    paragraph.innerText = content;
+    return paragraph;
+}
+
+//  3. Data to render
+let data = [
+    'Welcome to Ramar\'s Food. Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptates maxime animi natus officiis?',
+    'Our Menu,  Nihil ducimus sapiente ut corporis voluptatibus optio inventore adipisci.Quibusdam perspiciatis neque accusamus nam ? Ratione, quaerat placeat.',
+    'Nihil ducimus sapiente ut corporis voluptatibus optio inventore adipisci.Quibusdam perspiciatis neque accusamus nam ? Ratione, quaerat placeat.'
+];
+
+//  4. Function to render page 
+
+function renderHomePage() {
+    data.forEach(item => {
+        container.appendChild(paragraph(item));
+    });
+    return container;
+}
+
+export default renderHomePage;
